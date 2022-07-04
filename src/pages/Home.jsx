4d3fced2774846690cebbd17337a013c79bb7f80
dev-redo <mis05041@naver.com>
2022-07-05 02:39:05 +0900

    Style: Layout 컴포넌트 경로 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useUserState } from '../store/auth/provider';
-import Layout from '../components/base/Layout';
+import Layout from '../layout';
 import Feeds from '../components/feed/Feeds';
 import Axios from 'axios';
 
