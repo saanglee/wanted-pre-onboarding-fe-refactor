075d812446869ba2b5038a96c9805e952cea094a
sangji <sangllee20@gmail.com>
2022-07-06 02:23:54 +0900

    Fix: fix input field so that it doesn't change border color while entering

--- src/components/member/LoginForm.jsx
+++ src/components/member/LoginForm.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useRef } from 'react';
 import { useAuth } from '../../hooks/useAuth';
 
-const LoginForm = () => {
+import cx from 'classnames';
+
+const LoginForm = ({ isModal }) => {
   const { loginCallback } = useAuth();
 
   const [inputs, setInputs] = useState({
@@ -15,44 +17,63 @@ const LoginForm = () => {
   const inputPw = useRef();
 
   //이메일, 비밀번호 유효성 검사 상태 저장
-  const [statusId, setStatusId] = useState(true);
-  const [statusEmail, setStatusEmail] = useState(true);
-  const [statusPw, setStatusPw] = useState(true);
+  const [validName, setValidName] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPwd, setValidPwd] = useState(false);
+
+  const [nameFocused, setNameFocused] = useState(false);
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [pwdFocused, setPwdFocused] = useState(false);
+
   const [statusBtn, setstatesBtn] = useState(false);
+
   //비구조화 할당
   const { user, email, password } = inputs;
 
+  // Handle Focus
+  const handleEmailFocused = () => {
+    setEmailFocused(current => !current);
+  };
+
+  const handlePwdFocused = () => {
+    setPwdFocused(current => !current);
+  };
+
+  const handleNameFocused = () => {
+    setNameFocused(current => !current);
+  };
+
   //input change event
   const onChangeHandler = e => {
-    const idCurrent = inputId.current.value;
-    const emailCurrent = inputEmail.current.value;
-    const pwCurrent = inputPw.current.value;
+    const CurentName = inputId.current.value;
+    const CurrentEmail = inputEmail.current.value;
+    const CurrentPwd = inputPw.current.value;
 
-    if (idCurrent !== '') {
-      setStatusId(true);
+    if (CurentName !== '') {
+      setValidName(true);
     } else {
-      setStatusId(false);
+      setValidName(false);
     }
 
     //이메일 유효성
     if (validationEmail(email)) {
-      setStatusEmail(true);
+      setValidEmail(true);
     } else {
-      setStatusEmail(false);
+      setValidEmail(false);
     }
 
     //비밀번호 유효성
-    if (validationPassword(pwCurrent)) {
-      setStatusPw(true);
+    if (validationPassword(CurrentPwd)) {
+      setValidPwd(true);
     } else {
-      setStatusPw(false);
+      setValidPwd(false);
     }
 
     //이메일 && 비밀번호 유효성 검사 - 버튼 disabled 여부
     if (
-      idCurrent !== '' &&
-      validationEmail(emailCurrent) &&
-      validationPassword(pwCurrent)
+      CurentName !== '' &&
+      validationEmail(CurrentEmail) &&
+      validationPassword(CurrentPwd)
     ) {
       setstatesBtn(true);
     } else {
@@ -67,7 +88,6 @@ const LoginForm = () => {
     });
   };
 
-  // TODO: login 처리 로직 분리
   const onSubmitHandler = e => {
     e.preventDefault();
     loginCallback(inputs);
@@ -102,39 +122,65 @@ const LoginForm = () => {
     return true;
   }
 
+  // Button
+  const loginButton = (
+    <button
+      type="submit"
+      className="btn btn-point w100"
+      disabled={statusBtn ? '' : 'disabled'}
+    >
+      로그인
+    </button>
+  );
+  const signUpButton = (
+    <button
+      type="submit"
+      className="btn btn-point w100"
+      disabled={statusBtn ? '' : 'disabled'}
+    >
+      회원가입
+    </button>
+  );
+
   return (
     <form method="post" onSubmit={onSubmitHandler}>
       <input
         type="text"
         name="user"
         ref={inputId}
+        onFocus={handleNameFocused}
+        onBlur={handleNameFocused}
         onChange={onChangeHandler}
         placeholder="닉네임"
-        className={(statusId ? '' : 'error') + ' w100'}
+        className={cx('w100', {
+          error: validName !== '' && !validName && nameFocused,
+        })}
       />
       <input
         type="text"
         name="email"
         ref={inputEmail}
+        onFocus={handleEmailFocused}
+        onBlur={handleEmailFocused}
         onChange={onChangeHandler}
         placeholder="이메일"
-        className={(statusEmail ? '' : 'error') + ' w100'}
+        className={cx('w100', {
+          error: validEmail !== '' && !validEmail && emailFocused,
+        })}
       />
       <input
         type="password"
         name="password"
         ref={inputPw}
+        onFocus={handlePwdFocused}
+        onBlur={handlePwdFocused}
         onChange={onChangeHandler}
         placeholder="비밀번호"
-        className={(statusPw ? '' : 'error') + ' w100'}
+        className={cx('w100', {
+          error: validPwd !== '' && !validPwd && pwdFocused,
+        })}
       />
-      <button
-        type="submit"
-        className="btn btn-point w100"
-        disabled={statusBtn ? '' : 'disabled'}
-      >
-        로그인
-      </button>
+      {isModal ? signUpButton : loginButton}
     </form>
   );
 };
