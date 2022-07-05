37714740df5228eb3779f993451b43061608aa8d
dev-redo <mis05041@naver.com>
2022-07-06 01:53:58 +0900

    refactor: remove the redirect applied to each page
    기존에는 각 페이지에서 토큰 여부에 따라 임의의 페이지로 리다이렉트 시켰다. 하지만 라우팅 및 리다이렉트를 routes 파일에서 처리해주므로 각 페이지의 리다이렉트를 제거해주었다.
    
    close #20

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,57 +6,51 @@ import LoginForm from '../components/member/LoginForm';
 import { FaFacebookSquare } from 'react-icons/fa';
 
 const Login = () => {
-  const { token } = useUserState();
-
-  // TODO: Routes 폴더에서 라우팅 처리해주기
   return (
-    <>
-      {token && <Navigate to="/" replace={true} />}
-      <Layout>
-        <div className="login">
-          <div className="login-content">
-            <div className="login-form">
-              <h2>
+    <Layout>
+      <div className="login">
+        <div className="login-content">
+          <div className="login-form">
+            <h2>
+              <img
+                src={require('../assets/images/logo.png')}
+                alt=""
+              />
+            </h2>
+            <LoginForm />
+            <div className="login-hr">
+              <span></span>
+              <span>또는</span>
+              <span></span>
+            </div>
+            <div className="login-facebook">
+              <FaFacebookSquare />
+              <Link to="/">Facebook으로 로그인</Link>
+            </div>
+            <Link to="/">비밀번호를 잊으셨나요?</Link>
+          </div>
+          <div className="login-account">
+            <span>계정이 없으신가요?</span>
+            <Link to="/">가입하기</Link>
+          </div>
+          <div className="login-download">
+            <p>앱을 다운로드 하세요.</p>
+            <div className="apps">
+              <Link to="/">
                 <img
-                  src={require('../assets/images/logo.png')}
-                  alt=""
-                />
-              </h2>
-              <LoginForm />
-              <div className="login-hr">
-                <span></span>
-                <span>또는</span>
-                <span></span>
-              </div>
-              <div className="login-facebook">
-                <FaFacebookSquare />
-                <Link to="/">Facebook으로 로그인</Link>
-              </div>
-              <Link to="/">비밀번호를 잊으셨나요?</Link>
-            </div>
-            <div className="login-account">
-              <span>계정이 없으신가요?</span>
-              <Link to="/">가입하기</Link>
-            </div>
-            <div className="login-download">
-              <p>앱을 다운로드 하세요.</p>
-              <div className="apps">
-                <Link to="/">
-                  <img
-                    src={require('../assets/images/download_ios.png')}
-                  ></img>
-                </Link>
-                <Link to="/">
-                  <img
-                    src={require('../assets/images/download_and.png')}
-                  ></img>
-                </Link>
-              </div>
+                  src={require('../assets/images/download_ios.png')}
+                ></img>
+              </Link>
+              <Link to="/">
+                <img
+                  src={require('../assets/images/download_and.png')}
+                ></img>
+              </Link>
             </div>
           </div>
         </div>
-      </Layout>
-    </>
+      </div>
+    </Layout>
   );
 };
 
