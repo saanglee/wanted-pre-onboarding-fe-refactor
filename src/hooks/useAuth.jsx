5d97a055944e37831ff716f837d64f3336555b41
dev-redo <mis05041@naver.com>
2022-07-06 13:53:44 +0900

    Feat: Implement the user sign-up function
    유저가 로그인 시 입력값에 따라 아래 flow를 처리 해준다.
    
    - 존재하지 않는 id일 시, 회원가입 처리
    - 존재하는 id이나 pw를 잘못 기입 시, 비밀번호 확인 alert 띄우기
    - id와 pw가 일치하는 유저가 있을 시, 로그인 처리
    
    close #25

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom';
-import { useUserDispatch } from '../store/auth/provider';
-import { checkIsVerifyAccount } from '../util/checkIsVerifyAccount';
+import {
+  useUserDispatch,
+  useUserState,
+} from '../store/auth/provider';
+import { checkAccount } from '../util/checkAccount';
 
 /**
  * useAuth
@@ -11,21 +14,37 @@ import { checkIsVerifyAccount } from '../util/checkIsVerifyAccount';
  */
 export function useAuth() {
   const navigate = useNavigate();
+  const { userList } = useUserState();
   const dispatch = useUserDispatch();
 
-  const loginCallback = getLoginCallback(navigate, dispatch);
+  const signUpCallback = getSignUpCallback(dispatch);
+  const loginCallback = getLoginCallback(
+    navigate,
+    userList,
+    dispatch,
+    signUpCallback,
+  );
   const logoutCallback = getLogoutCallback(navigate, dispatch);
 
   return { loginCallback, logoutCallback };
 }
 
 const getLoginCallback =
-  (navigate, dispatch) => async inputValues => {
+  (navigate, userList, dispatch, signUpCallback) =>
+  async inputValues => {
     const { user, email, password } = inputValues;
-    const isUserExist = checkIsVerifyAccount({ email, password });
+    const { isUserExist, isUserValid } = checkAccount(
+      { email, password },
+      userList,
+    );
 
-    if (!isUserExist) {
-      alert('아이디 또는 비밀번호를 잘못 입력하셨습니다!');
+    if (isUserExist && !isUserValid) {
+      alert('비밀번호가 잘못되었습니다! 확인해주세요!');
+      return;
+    }
+
+    if (!(isUserExist && isUserValid)) {
+      signUpCallback({ email, password });
       return;
     }
 
@@ -45,4 +64,15 @@ const getLogoutCallback = (navigate, dispatch) => async () => {
   });
 
   navigate('/login', { replace: true });
+};
+
+const getSignUpCallback = dispatch => async user => {
+  alert('존재하지 않는 유저입니다. 회원가입 처리 하겠습니다');
+
+  dispatch({
+    type: 'SIGNUP',
+    user,
+  });
+
+  alert('회원가입 완료했습니다! 로그인 해주세요!');
 };
